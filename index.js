c8c40841d347500ae1aba4606a5375217ee1fcea
rvrutan <rutanroni@gmail.com>
2024-12-02 16:32:39 -0500

    completed

--- index.js
+++ index.js
@@ -1,13 +1,87 @@
 // TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from "fs";
+
+//internal
+import generateMarkdown from "./utils/generateMarkdown.js";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+    default: "Project Title",
+  },
+  {
+    type: "input",
+    message: "Write a description of your project.",
+    name: "description",
+    default: "Project Description",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the usage information?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What are guidelines for contribution?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What are the test instructions?",
+  },
+  {
+    type: "list",
+    message: "Choose a license for your project.",
+    name: "license",
+    choices: [
+      "Apache 2.0",
+      "MIT",
+      "Zlib",
+      "WTFPL",
+      "None",
+    ],
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        console.log('Generating README...'); 
+        const markdownContent = generateMarkdown(answers);
 
+        fs.writeFile('README.md', markdownContent, (err) =>
+        err 
+        ? console.error(colors.red('Error while writing file.', err))
+        : console.log('Success! README.md generated.')
+        );
+
+    }).catch((error) => {
+        console.error('Initialization error.', error);
+    });
+}
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,91 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license || license === "None") {
+    return "";
+  }
+  switch (license) {
+    case "MIT":
+      return "![License: MIT](https://img.shields.io/badge/license-MIT-blue)";
+    case "Apache 2.0":
+      return "![License: Apache 2.0](https://img.shields.io/badge/license-Apache%202.0-blue?style=flat-square)";
+    case "Zlib":
+      return "![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)";
+    case "WTFPL":
+      return "![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license || license === "None") {
+    return "";
+  }
+
+  switch (license) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "Apache 2.0":
+      return "https://www.apache.org/licenses/LICENSE-2.0";
+    case "Zlib":
+      return "https://www.zlib.net/zlib_license.html";
+    case "WTFPL":
+      return "http://www.wtfpl.net/about/";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license || license === "None") {
+    return "";
+  }
+  return `License: ${renderLicenseLink(license)}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `## Title
+  # ${data.title}
+  ${renderLicenseBadge(data.license)}
 
+  ## Description
+${data.description}
+
+## Table of Contents
+-[Description] (#description)
+-[Installation](#installation)
+-[Usage](#usage)
+-[License](#license)
+-[Contributing](#contributing)
+-[Test](#test)
+-[Questions](#questions)
+
+##Installation
+${data.installation}
+
+##Usage
+${data.usage}
+
+##License
+${renderLicenseSection(data.license)}
+
+##Contributing
+${data.contributing}
+
+##Test
+${data.test}
+
+##Questions
+
+Contact me for any further questions;
+-Github: [${data.github}] (https://github.com/${data.username})
+-Email: [${data.email}](mailto${data.email})
 `;
 }
 
